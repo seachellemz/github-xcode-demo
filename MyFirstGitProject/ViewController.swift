5781bf5521bddf9f64b720fb0999a54134bdc060
Michelle Zaremskas <mzaremskas@comcast.net>
2015-12-05 13:42:32 -0500

    My First Commit

--- MyFirstGitProject/ViewController.swift
+++ MyFirstGitProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("I can't wait to push this project to GitHub")
+        print("I can't wait to push this project to GitHub2")
     }
 
     override func didReceiveMemoryWarning() {
